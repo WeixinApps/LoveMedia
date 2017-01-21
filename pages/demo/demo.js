ace9b668b590482ef6e528602d1b86a1080e5a86
zdt@macbook <dev@dthappy.com>
2017-01-22 00:19:02 +0800

    change demo with ES6

--- pages/demo/demo.js
+++ pages/demo/demo.js
@@ -1,9 +1,21 @@
-Page({
-    onShareAppMessage: function () {
+class Demo{
+  constructor(){
+    this.bindMethods();
+  }
+  bindMethods(){
+    this.showNewPage = this.showNewPage.bind(this);
+  }
+  showNewPage(){
+    wx.navigateTo({
+      url:'../logs/logs'
+    });
+  }
+  onShareAppMessage(){
     return {
       title: '自定义分享标题',
       desc: '自定义分享描述',
       path: '/page/demo?id=123'
     }
   }
-})
+}
+Page(new Demo());
